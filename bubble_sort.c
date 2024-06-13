eabfdd317c533092a6edfd740efb28969d7bafef
gyojikato <112055645+gyojikato@users.noreply.github.com>
2024-06-13 20:29:20 +0800

    Create bubble_sort.c

---
+++ bubble_sort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *num1, int *num2)
+{
+    int temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+
+}
+void bubble_sort(int *array, unsigned int size)
+{
+    int swap_count = 0;
+    for(int i = 0; i < size - 1; i++)
+    {
+        if(array[i] < array[i + 1])
+        {
+            swap(&array[i], &array[i + 1]);
+            swap_count = 1;
+        }
+    }
+    if(swap_count)
+    {
+        bubble_sort(array, size - 1);
+    }
+}
+
+void dispArr(int *arr, int size)
+{
+    printf("elements: ");
+    for(int i = 0; i < size; i++)
+    {
+        printf("%d, ", *(arr + i));
+    }
+    printf("\n");
+}
+int main()
+{
+    int array[] = {2, 3, 2, 2, 1};
+    dispArr(array, sizeof(array)/sizeof(int));
+    bubble_sort(array, sizeof(array)/sizeof(int));
+    dispArr(array, sizeof(array)/sizeof(int));
+}
